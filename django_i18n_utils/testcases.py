a747e670872b78a1068553b0d42cc68e62745f57
Chris Adams <chris@improbable.org>
2014-05-28 17:03:54 -0400

    Consistent encoding across Python 2/3

--- django_i18n_utils/testcases.py
+++ django_i18n_utils/testcases.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf import settings
 from django.test import TestCase
