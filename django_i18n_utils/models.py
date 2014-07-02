5496241c0aaa68d303e8e19844615310fffcefad
Chris Adams <chris@improbable.org>
2014-07-02 11:15:52 -0400

    Add unicode_safe_repr class decorator
    This provides a simple, safe way to have __repr__ return
    values which have Unicode characters escaped using the
    unicode_escape encoding

--- django_i18n_utils/models.py
+++ django_i18n_utils/models.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 from __future__ import absolute_import, print_function, unicode_literals
 
+from functools import update_wrapper
+
 from django.core.exceptions import ValidationError
 from django.db import models
 
@@ -49,3 +51,47 @@ class UnicodeNormalizerMixin(object):
 
         if errors:
             raise ValidationError(errors)
+
+
+def unicode_safe_repr(format_string, field_names):
+    """Class decorator which ensures that repr will return a Unicode-escaped safe for ASCII consumers
+
+    Requires a format string and a list of class attributes to be included for formatting. All strings will
+    be encoded using the `unicode_escape` encoding. Values which have a `pk` attribute – e.g. Django models –
+    will be processed using `repr()` so this must be applied to all of your foreign-key classes as well.
+
+    Usage::
+
+        @unicode_safe_repr(u'{class_name}(title=u"{title}")', ('title', ))
+        class MyDjangoModel(…):
+            …
+    """
+
+    assert isinstance(format_string, unicode), 'format_string must be Unicode!'
+
+    def inner(cls):
+        def safe_repr(self):
+            field_values = []
+
+            for k in field_names:
+                v = getattr(self, k)
+
+                if hasattr(v, 'pk'):
+                    field_values.append(repr(v))
+                elif isinstance(v, basestring):
+                    field_values.append(v.encode('unicode_escape'))
+                else:
+                    field_values.append(v)
+
+            values = {'class_name': cls.__name__}
+            values.update(zip(field_names, field_values))
+            return format_string.format(**values)
+
+        update_wrapper(safe_repr, cls.__repr__)
+        cls.__repr__ = safe_repr
+
+        return cls
+
+    return inner
+
+
